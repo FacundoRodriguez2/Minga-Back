2b923f551f3ff0070d4be3983debc013e25acd03
Agustin <pereyra.agus@outlook.com.ar>
2023-03-02 16:21:12 -0300

    fixed

--- schemas/users/usersSignIn.js
+++ schemas/users/usersSignIn.js
@@ -5,12 +5,19 @@ const schema = Joi.object({
         .string()
         .required()
         .min(8)
-        .email({ minDomainSegments: 2  }),
+        .email({ minDomainSegments: 2  })
+        .messages({
+            'string.min':'the mail must be at least 8 characters long'
+        }),
     password: Joi
         .string()
         .required()
         .min(8)
-        .max(50),
+        .max(50)
+        .messages({
+            'string.min':'the password must be at least 8 characters long',
+            'string.max':'the password must have a maximum of 20 characters'
+        }),
 })
 
 export default schema
