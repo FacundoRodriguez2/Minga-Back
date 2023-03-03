532eba8e235f6bfdaaa72bab0fbba172e1516080
Facundo Rodriguez <48458428+FacundoRodriguez2@users.noreply.github.com>
2023-03-03 11:25:18 -0300

    middle+joi (#10)
    * middle+joi
    
    * fix

--- routes/chapters.js
+++ routes/chapters.js
@@ -1,16 +1,19 @@
 import express from 'express'
-import Chapter from '../models/Chapter.js'
-import controller from '../controllers/chapters/create.js';
+import createController from '../controllers/chapters/create.js';
+import Schema from '../schemas/chapters/chapters.js'
+import validator from '../middlewares/validator.js';
+import nextOrder from '../middlewares/chapters/next_order.js';
+import addFrontPhoto from '../middlewares/chapters/add_front_photo.js';
+import existsOrder from '../middlewares/chapters/exists_order.js';
+import passport from '../middlewares/passport.js';
 let router = express.Router();
 
-const {create}=controller
 
 
-router.get('/', function (req, res, next) {
-    res.send('chapters here');
-});
+const {create}=createController
 
-router.post('/chapters',create)
+
+router.post('/',passport.authenticate('jwt',{session:false}),validator(Schema),existsOrder,nextOrder,addFrontPhoto,create )
 
 
 export default router
