da5c56cafbc08f9dd0a412ff959e3b0175c3f0d1
Juan Jose Ramirez B <juanjose.ramirezbo@gmail.com>
2023-03-06 17:09:41 -0500

    form manga

--- controllers/manga/create.js
+++ controllers/manga/create.js
@@ -4,14 +4,13 @@ const controller = {
     create: async (req, res) => {
         try {
           let manga = await Manga.create(req.body);
-          req.body.author_id = "63fe8112f09373806fd89fe5";
-          return res.status(200).json({
+          return res.status(201).json({
             response: "created",
             manga: req.body,
           });
         } catch (error) {
-          return res.status(200).json({
-            response: "error.message",
+          return res.status(400).json({
+            response: error.message,
           });
         }
       }
--- models/Manga.js
+++ models/Manga.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 let schema = new mongoose.Schema(
   {
-    title: { type: String, required: true},
+    tittle: { type: String, required: true},
+    category: { type: String, required: true},
     description: { type: String, required: true },
     cover_photo: { type: String, required: true },
     author_id: { type: String, required: true,  ref: "authors" },
