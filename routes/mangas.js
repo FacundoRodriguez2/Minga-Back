fea7fe454e9d74f6b4a044eab19082de7151fb26
Juanjoseramib <123500236+Juanjoseramib@users.noreply.github.com>
2023-03-06 17:39:53 -0500

    form manga (#14)

--- routes/mangas.js
+++ routes/mangas.js
@@ -1,6 +1,7 @@
 import express from "express";
 import createcontroller from "../controllers/manga/create.js"
 import showcontroller from "../controllers/categories/show.js"
+import existsTittle from "../middlewares/exists_tittle.js"
 import passport from "../middlewares/passport.js";
 
 let router = express.Router();
@@ -8,7 +9,8 @@ let {create} = createcontroller
 let {show}= showcontroller
 
 router.get("/", show)
-router.post("/", passport.authenticate('jwt', { session: false }) ,create) 
+router.post("/", passport.authenticate('jwt', { session: false }), existsTittle ,create) 
+
 
 
 export default router;
