8e2eea46a443ee343179ec7acc1a3b69cdfe07b9
Juanjoseramib <123500236+Juanjoseramib@users.noreply.github.com>
2023-03-09 11:31:07 -0500

    pages sin datos sencibles y ordenadas (#18)

--- routes/chapters.js
+++ routes/chapters.js
@@ -6,13 +6,15 @@ import nextOrder from '../middlewares/chapters/next_order.js';
 import addFrontPhoto from '../middlewares/chapters/add_front_photo.js';
 import existsOrder from '../middlewares/chapters/exists_order.js';
 import passport from '../middlewares/passport.js';
+import getOneChapterController from "../controllers/chapters/get_one.js"
 let router = express.Router();
 
 
+const {create}= createController
+const {showOne} = getOneChapterController
 
-const {create}=createController
 
-
+router.get('/:id', showOne)
 router.post('/',passport.authenticate('jwt',{session:false}),validator(Schema),existsOrder,nextOrder,addFrontPhoto,create )
 
 
