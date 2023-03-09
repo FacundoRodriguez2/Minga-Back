c9d6098f0b1fd3b0e8dbaea30ac8c0f71b996d17
facundo rodriguez <facundo2punto0@gmail.com>
2023-03-09 17:18:31 -0300

    actualizado

--- routes/chapters.js
+++ routes/chapters.js
@@ -7,24 +7,11 @@ import addFrontPhoto from '../middlewares/chapters/add_front_photo.js';
 import existsOrder from '../middlewares/chapters/exists_order.js';
 import passport from '../middlewares/passport.js';
 import getChapter from '../controllers/chapters/get_chapter.js'
-<<<<<<< HEAD
-
-=======
 import getOneChapterController from "../controllers/chapters/get_one.js"
->>>>>>> e7afb3e13e1e63b46faffb8f376fad4e5ce96b86
 
 let router = express.Router();
 const {create}=createController
 const {get}=getChapter
-<<<<<<< HEAD
-
-
-
-router.get('/',get)
-router.post('/',passport.authenticate('jwt',{session:false}),validator(Schema),existsOrder,nextOrder,addFrontPhoto,create)
-
-
-=======
 const {showOne} = getOneChapterController
 
 
@@ -32,7 +19,6 @@ router.get('/',get)
 router.get('/:id', showOne)
 router.post('/',passport.authenticate('jwt',{session:false}),validator(Schema),existsOrder,nextOrder,addFrontPhoto,create)
 
->>>>>>> e7afb3e13e1e63b46faffb8f376fad4e5ce96b86
 
 
 export default router
