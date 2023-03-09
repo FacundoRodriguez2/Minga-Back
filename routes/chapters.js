6a8879728cf19d2a2b75e62f9b55c970b4931fe5
facundo rodriguez <facundo2punto0@gmail.com>
2023-03-09 15:41:38 -0300

    fixes

--- routes/chapters.js
+++ routes/chapters.js
@@ -7,7 +7,7 @@ import addFrontPhoto from '../middlewares/chapters/add_front_photo.js';
 import existsOrder from '../middlewares/chapters/exists_order.js';
 import passport from '../middlewares/passport.js';
 import getChapter from '../controllers/chapters/get_chapter.js'
-
+import getOneChapterController from "../controllers/chapters/get_one.js"
 
 let router = express.Router();
 const {create}=createController
@@ -18,7 +18,6 @@ const {showOne} = getOneChapterController
 router.get('/',get)
 router.get('/:id', showOne)
 router.post('/',passport.authenticate('jwt',{session:false}),validator(Schema),existsOrder,nextOrder,addFrontPhoto,create)
-
 
 
 
