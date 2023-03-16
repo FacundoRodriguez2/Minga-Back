f56e12f7742425cd26309ca9e08551bbcea48ca8
Eliana <guarinoeliana@gmail.com>
2023-03-16 16:24:09 -0300

    Controllers, Middlewares and Routes

--- controllers/companies/create.js
+++ controllers/companies/create.js
@@ -2,6 +2,8 @@ import  Company  from "../../models/Company.js"
 
 const controller = {
     create: async (req, res, next) => {
+        req.body.active = true
+        req.body.user_id = req.user._id
         try {
             const company = await Company.create(req.body)
             res.status(201).json({
