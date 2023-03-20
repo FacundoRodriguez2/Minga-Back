111b1e19982eabe5cc54e5fbf28196dc11253fde
Juan Jose Ramirez B <juanjose.ramirezbo@gmail.com>
2023-03-19 19:53:05 -0500

    get_me controller

---
+++ controllers/manga/get_me.js
@@ -0,0 +1,41 @@
+import Manga from "../../models/Manga.js";
+
+
+const controller = {
+    
+    get_me: async (req,res,next) => {
+        
+        try{
+            
+            let query = {}
+            
+            if (req.query.author_id) {
+                
+                query.author_id = req.body.author_id
+
+            }
+
+               console.log(query)
+    
+               const mangas = await Manga.find(query)
+                                         .select("_id author_id title cover_photo category_id ")
+                                         .populate("author_id", "name last_name -_id")
+                                         .populate("category_id", "name -_id")
+               
+
+               if(mangas){
+                return res.status(200).json({
+                    success: true,
+                    mangas
+                })
+               }
+               
+        }catch(error){
+            next(error)
+            console.log(error)
+        }
+        }
+    }
+
+
+export default controller 
--- routes/mangas.js
+++ routes/mangas.js
@@ -7,21 +7,26 @@ import passport from "../middlewares/passport.js";
 import get_mangas from "../controllers/manga/get_mangas_from_author.js"
 import get_manga from '../controllers/manga/get_one.js'
 import getMangas from '../controllers/manga/get_mangas.js'
+import get_data from "../controllers/manga/get_me.js"
+import finds_id from "../middlewares/finds_id.js"
 
 let router = express.Router();
 let {create} = createcontroller
 const {get_one}=get_manga
 const { get_mangas_from_author } = get_mangas
 const { read } = getMangas
+const { get_me } = get_data
 
 
 router.post("/", passport.authenticate('jwt', { session: false }), validator(schema), existsTitle ,create)
 
-router.get( "/authors/:author_id", passport.authenticate("jwt", { session: false }),get_mangas_from_author )
-    
-router.get('/:id',passport.authenticate("jwt", { session: false }),get_one)
+router.get( "/authors/:author_id", passport.authenticate("jwt", { session: false }), get_mangas_from_author )
 
-router.get('/', passport.authenticate('jwt', { session:false }), read)
+router.get("/me", passport.authenticate('jwt', { session: false }), finds_id, get_me)
+    
+router.get('/:id',passport.authenticate("jwt", { session: false }), get_one)
+
+router.get('/', passport.authenticate('jwt', { session: false }), read)
 
 
 export default router;
