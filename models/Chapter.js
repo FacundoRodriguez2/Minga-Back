d1e409cee2d2a8e28e6d274d98f79134551fe739
facundo rodriguez <facundo2punto0@gmail.com>
2023-03-22 21:37:09 -0300

    fixes de la base de datos

--- models/Chapter.js
+++ models/Chapter.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 const schema = new mongoose.Schema(
     {
         manga_id:{ type:mongoose.Types.ObjectId, ref:'mangas', required:true },
+        cover_photo:{ type:String, required:true },
         title: { type:String, required:true },
         pages: { type:Array, required:true },
         order: { type:Number }        
