9d3ffe2f523cf673b4d6993dd303553a28f6ea72
Facundo Rodriguez <48458428+FacundoRodriguez2@users.noreply.github.com>
2023-03-23 12:50:58 -0300

    Fixes (#35)
    * M-11 back
    
    * actualizado
    
    * fix
    
    * test+M14
    
    * M14
    
    * fixes de la base de datos
    
    ---------
    
    Co-authored-by: Eliana Stefani Guarino <119450786+stefy0075@users.noreply.github.com>

--- controllers/chapters/get_chapter.js
+++ controllers/chapters/get_chapter.js
@@ -3,15 +3,17 @@ import Chapter from "../../models/Chapter.js";
 const get_chapters={
         get: async (req, res, next) => {
         let chapters = {} 
-        let pagination = { page: 1,limit: 4}
+        let pagination = { page: 1}
         if (req.query.manga_id) {
                 chapters.manga_id = req.query.manga_id
         }
         if (req.query.page) {
                 pagination.page = req.query.page
+                pagination.limit=4
         }    
+
         try{
-                let chapter = await Chapter.find(chapters).select('title order manga_id -_id')
+                let chapter = await Chapter.find(chapters).select('title order manga_id _id cover_photo ')
                 .sort({order:1})
                 .skip(pagination.page > 0 ? (pagination.page - 1) * pagination.limit : 0)
                 .limit(pagination.limit > 0 ? pagination.limit : 0)
