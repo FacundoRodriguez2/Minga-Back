7e5862e77731491999fbc7a5f04e3be0294335fc
Juan Jose Ramirez B <juanjose.ramirezbo@gmail.com>
2023-03-25 18:56:10 -0500

    push porque cambio de pc

--- routes/categories.js
+++ routes/categories.js
@@ -1,11 +1,11 @@
 import express from 'express'
 import showcontroller from "../controllers/categories/show.js"
-import passport from "../middlewares/passport.js";
+
 
 const { all } = showcontroller
 
 let router = express.Router()
 
-router.get("/", passport.authenticate('jwt', { session: false }), all)
+router.get("/", all)
 
 export default router
