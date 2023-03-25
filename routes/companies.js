6e085a75ba320f0575bf8ad96f9d151c09f698fb
facundo rodriguez <facundo2punto0@gmail.com>
2023-03-25 14:24:08 -0300

    M-19

--- routes/companies.js
+++ routes/companies.js
@@ -1,13 +1,17 @@
 import controller from "../controllers/companies/create.js"
+import readController from '../controllers/companies/read_all.js'
 import schema  from "../schemas/companies/create.js"
 import express from "express"
-// import passport from "../middlewares/passport.js"
+import passport from "../middlewares/passport.js"
 import validator from "../middlewares/validator.js"
 
 const router = express.Router()
 
 const { create } = controller
+const {read_all} =readController
 
-router.post("/", validator( schema ), create)
+router.post("/",passport.authenticate('jwt', { session: false }), validator( schema ), create)
+router.get('/', read_all)
+
 
 export default router
