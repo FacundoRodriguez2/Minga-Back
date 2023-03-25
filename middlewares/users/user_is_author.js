f9c547b4c29229a78702524704248be32faf32ca
Eliana <guarinoeliana@gmail.com>
2023-03-25 14:59:12 -0300

    back funcionando

--- middlewares/users/user_is_author.js
+++ middlewares/users/user_is_author.js
@@ -1,11 +1,7 @@
 
 async function user_is_author(req,res,next) {
 
-<<<<<<< HEAD
     if (req.user.is_author === true) {
-=======
-    if (req.user.is_author) {
->>>>>>> bc5b1698a50a8030be301eef35e53fb5223c5180
         return next()
     }
     res.status(401).json({
@@ -14,4 +10,4 @@ async function user_is_author(req,res,next) {
     })
 }
 
-export default  user_is_author 
+export default  user_is_author
